20cf5bcd7eb38011ecd75d6358b9a450a5332f2f
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2019-03-21 12:19:27 +0100

    minor edit

--- scripts/dlc-tracking-2-inside-docker.py
+++ scripts/dlc-tracking-2-inside-docker.py
@@ -1,6 +1,11 @@
+import os
 import json
 import deeplabcut
 
+# Disable GUI
+os.environ["DLClight"]="True"
+
+# Load videos and config file
 with open('settings.json') as json_file:  
     settings = json.load(json_file)
     
