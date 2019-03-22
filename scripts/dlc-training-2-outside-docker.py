ba8ac8500f8b0631ca511627bf4671ebc57e08d4
Aleksejs Fomins <aleksejs.fomins@uzh.ch>
2019-03-22 12:00:32 +0100

    editing dlc paths automatic - 2

---
+++ scripts/dlc-training-2-outside-docker.py
@@ -0,0 +1,96 @@
+'''
+  The purpose of this code is to adjust all paths in the training dataset
+'''
+
+import numpy as np
+import os, sys
+import yaml
+import pathlib
+import h5py
+import pandas as pd
+
+pwd_project = sys.argv[1]
+
+pwd_config_file = os.path.join(pwd_project, 'config.yaml')
+
+if not os.path.isfile(pwd_config_file):
+  raise ValueError('Config file not found at:', pwd_config_file)
+
+pwd_project     = os.path.abspath(pwd_project)
+pwd_config_file = os.path.abspath(pwd_config_file)
+
+#print("Absolute path to project is: ", pwd_project)
+print("Absolute path to project file is: ", pwd_config_file)
+
+##################################
+#  Editing config.yaml file
+##################################
+print('Editing config.yaml file')
+with open(pwd_config_file, "r") as fconf:
+  data_conf = yaml.load(fconf)
+
+# Fix project path
+project_path_old = data_conf['project_path']
+data_conf['project_path'] = pwd_project
+#print('Old path:', project_path_old)
+
+# Fix all paths to videos
+video_sets_old = data_conf.pop('video_sets')
+data_conf['video_sets'] = {}
+for k,v in video_sets_old.items():
+  k_new = k.replace(project_path_old, data_conf['project_path']).replace('\\','/')
+  data_conf['video_sets'][k_new] = v
+
+# Save changes
+with open(pwd_config_file, 'w') as fconf_new:
+  yaml.dump(data_conf, fconf_new, default_flow_style=False)
+
+##################################
+#  Editing csv paths
+##################################
+print('Editing paths in .csv marking files')
+pwd_labeled = os.path.join(pwd_project, 'labeled-data')
+for dirpath, dirnames, filenames in os.walk(pwd_labeled):
+    for filename in [f for f in filenames if f.endswith(".csv")]:
+        csv_fname = os.path.join(dirpath, filename)
+        print("Fixing file", csv_fname)
+
+        with open(csv_fname, 'r') as csvfile:
+            data_csv = csvfile.readlines()
+            
+        data_csv = [line.strip().split(',') for line in data_csv]
+        data_csv = [[line[0].replace('\\', '/')] + line[1:] for line in data_csv]
+            
+        with open(csv_fname, 'w') as csvfile:
+            for line in data_csv:
+                csvfile.write(",".join(line) + '\n')
+
+##################################
+#  Marking h5 paths
+##################################
+#print('Editing paths in .h5 marking files')
+#pwd_labeled = os.path.join(pwd_project, 'labeled-data')
+#for dirpath, dirnames, filenames in os.walk(pwd_labeled):
+    #for filename in [f for f in filenames if f.endswith(".h5")]:
+        #h5_fname = os.path.join(dirpath, filename)
+        #print("Fixing file", h5_fname)
+        
+        #h5_df = pd.read_hdf(h5_fname, 'df_with_missing')
+        #print(h5_df.keys())
+        
+        ##h5_df.to_hdf(h5_fname, 'df_with_missing', format='table', mode='w')
+        
+        ##h5f = h5py.File(h5_fname, 'r+')
+        ##sub = h5f['df_with_missing']
+        
+        ##data_h5 = list(sub['table'])
+        ##data_h5 = [(line[0].decode('UTF-8').replace('\\', '/').encode(), line[1]) for line in data_h5]
+        ##sub['table'] = data_h5
+        ##h5f.close()
+        
+        
+        ##for line in data_h5:
+            ##print(line)
+            ###print([line[0].decode('UTF-8').replace('\\', '/')] + line[1:])
+
+        
